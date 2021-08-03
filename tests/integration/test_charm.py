3cfe7dca3629a4f53bf00ebbd8c17d80899830e9
sed-i <82407168+sed-i@users.noreply.github.com>
2021-08-03 15:56:38 -0400

    reorg tests (integration still broken)

---
+++ tests/integration/test_charm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+#  Copyright 2021 Canonical Ltd.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import logging
+from pathlib import Path
+
+import yaml
+
+import pytest
+
+log = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test):
+    my_charm = await ops_test.build_charm(".")
+    resources = {"placeholder-image": "alpine"}
+    # resources={"placeholder-image": "busybox"}
+    await ops_test.model.deploy(my_charm, resources=resources)
+    await ops_test.model.wait_for_idle()
+
+
+async def test_status(ops_test):
+    charm_name = METADATA["name"]
+    assert ops_test.model.applications[charm_name].units[0].workload_status == "active"
