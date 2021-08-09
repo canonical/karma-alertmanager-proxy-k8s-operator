694e15162ca1827c526fd1143f9e316d92fde5d3
sed-i <82407168+sed-i@users.noreply.github.com>
2021-08-09 14:56:37 -0400

    use address correctly

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -32,11 +32,30 @@ class TestClass:
         charm_under_test = await ops_test.build_charm(".")
         resources = {"placeholder-image": "alpine"}
         self.proxy_app = await ops_test.model.deploy(
-            charm_under_test,
-            resources=resources,
-            application_name="proxy")
+            charm_under_test, resources=resources, application_name="proxy"
+        )
         await ops_test.model.wait_for_idle(apps=["proxy"])
         assert ops_test.model.applications["proxy"].units[0].workload_status == "blocked"
+
+        async def cli_deploy_and_wait(
+            name: str, alias: str = "", wait_for_status: str = None, channel="edge"
+        ):
+            if not alias:
+                alias = name
+            retcode, stdout, stderr = await ops_test._run(
+                "juju",
+                "deploy",
+                "-m",
+                ops_test.model_full_name,
+                name,
+                alias,
+                f"--channel={channel}",
+            )
+            assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
+            log.info(stdout)
+            await ops_test.model.wait_for_idle(
+                apps=[alias], wait_for_status=wait_for_status, timeout=60
+            )
 
         # also deploy alertmanager from charmhub
         # self.am_app = await ops_test.model.deploy(
@@ -45,12 +64,7 @@ class TestClass:
         #     channel='edge',
         # )
         # use CLI to deploy bundle until https://github.com/juju/python-libjuju/issues/511 is fixed.
-        retcode, stdout, stderr = await ops_test._run(
-            "juju", "deploy", "-m", ops_test.model_full_name, "alertmanager-k8s", "am", "--channel=edge"
-        )
-        assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
-        log.info(stdout)
-        await ops_test.model.wait_for_idle(apps=["am"], wait_for_status="active", timeout=60)
+        await cli_deploy_and_wait("alertmanager-k8s", "am", "active")
         assert ops_test.model.applications["am"].units[0].workload_status == "active"
 
         # also karma alertmanager from charmhub
@@ -59,39 +73,29 @@ class TestClass:
         #     application_name='karma',
         #     channel='edge',
         # )
-
-        retcode, stdout, stderr = await ops_test._run(
-            "juju", "deploy", "-m", ops_test.model_full_name, "karma-k8s", "karma", "--channel=edge"
-        )
-        assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
-        log.info(stdout)
-        await ops_test.model.wait_for_idle(apps=["karma"], wait_for_status="blocked")
+        await cli_deploy_and_wait("karma-k8s", "karma", "blocked")
         assert ops_test.model.applications["karma"].units[0].workload_status == "blocked"
 
         # due to a juju bug, occasionally alertmanager finishes a startup sequence with "waiting for IP address"
         # issuing a dummy config change just to trigger an event
         await ops_test.model.applications["am"].set_config({"pagerduty_key": "just_a_dummy"})
-        #await ops_test.model.wait_for_idle(apps=["am"], wait_for_active=True, timeout=60)
+        await ops_test.model.wait_for_idle(apps=["am"], wait_for_status="active", timeout=60)
 
-    @pytest.mark.skip(reason="one at a time please")
     @pytest.mark.abort_on_fail
     async def test_config_proxy_with_alertmanager_ip(self, ops_test):
-        # TODO relate the apps, and verify relations are valid
-        log.info("================================================")
-        log.info("===== OMG I'M FINALLY HERE =====================")
-        log.info("================================================")
-
-        url = f"http://{ops_test.model.applications['am'].unit[0].public_address}:9093"
+        # url = f"http://{ops_test.model.applications['am'].units[0].data['private-address']}:9093"
+        status = await ops_test.model.get_status()
+        url = f"http://{status['applications']['am']['units']['am/0']['address']}:9093"
         log.info("am public address: %s", url)
-        await self.proxy_app.set_config({"url": url})
-        await ops_test.model.wait_for_idle(wait_for_status="active", timeout=60)
-        assert self.proxy_app.units[0].workload_status == "active"
+        await ops_test.model.applications["proxy"].set_config({"url": url})
+
+        await ops_test.model.wait_for_idle(apps=["proxy"], wait_for_status="active", timeout=60)
+        assert ops_test.model.applications["proxy"].units[0].workload_status == "active"
 
     @pytest.mark.skip(reason="one at a time please")
     @pytest.mark.abort_on_fail
-    async def test_relations_to_karma(self, ops_test):
+    async def test_relation_to_karma(self, ops_test):
         await ops_test.model.add_relation("proxy", "karma")
-        await ops_test.model.wait_for_idle()
+        await ops_test.model.wait_for_idle(wait_for_status="active")
         log.info(ops_test.model.relations)
         assert ops_test.model.applications["karma"].units[0].workload_status == "active"
-
