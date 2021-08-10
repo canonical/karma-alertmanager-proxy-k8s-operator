a61b30c81d25c3f1a5870e76f928853d1761a565
sed-i <82407168+sed-i@users.noreply.github.com>
2021-08-09 22:49:20 -0400

    add unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -6,13 +6,26 @@
 import unittest
 from charm import AlertmanagerKarmaProxyCharm
 from ops.testing import Harness
+from ops.model import ActiveStatus, BlockedStatus
 
 
 class TestCharm(unittest.TestCase):
     def setUp(self):
         self.harness = Harness(AlertmanagerKarmaProxyCharm)
         self.addCleanup(self.harness.cleanup)
-        self.harness.begin()
 
-    def test_dummy(self):
-        self.assertTrue(True)
+        self.harness.set_leader(True)
+        self.harness.begin_with_initial_hooks()
+
+    def test_unit_status_without_config(self):
+        self.assertIsInstance(self.harness.charm.unit.status, BlockedStatus)
+
+    def test_unit_status_with_config(self):
+        self.harness.update_config({"url": "http://1.2.3.4:5678"})
+        self.assertIsInstance(self.harness.charm.unit.status, ActiveStatus)
+
+    @unittest.skip("not yet implemented")
+    def test_status_returns_to_blocked(self):
+        self.harness.update_config({"url": "http://1.2.3.4:5678"})
+        self.harness.update_config({"url": ""})
+        self.assertIsInstance(self.harness.charm.unit.status, BlockedStatus)
