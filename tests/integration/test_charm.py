ad51e0d8118e3f090918ea89d21fa42229a0b7e8
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-11 16:53:38 -0500

    fix async invocation

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -29,7 +29,7 @@ async def test_build_and_deploy(ops_test, charm_under_test):
 @pytest.mark.abort_on_fail
 async def test_charm_goes_into_active_state_after_alertmanager_ip_provided(ops_test):
     # deploy alertmanager
-    with IPAddressWorkaround(ops_test):
+    async with IPAddressWorkaround(ops_test):
         await ops_test.model.deploy("ch:alertmanager-k8s", application_name="am", channel="edge")
         await ops_test.model.wait_for_idle(apps=["am"], status="active")
 
